566b328a347c7c3ba30c0ebb2d9a6b39ccb20c4e
pokou2020 <61155644+pokou2020@users.noreply.github.com>
2021-01-13 11:20:26 +0100

    10h20

--- lib/babiflix/provider/films.dart
+++ lib/babiflix/provider/films.dart
@@ -1,23 +1,23 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-import 'film.dart';
+// import 'film.dart';
 
-class Movies with ChangeNotifier {
-  List<Films> _filmList = [];
-  void addFilm(Films film) {
-    final newFilm = Films(
-      id: DateTime.now().toString(),
-      titre: film.titre,
-      descrip: film.descrip,
-      image: film.image,
-      realisateur: film.realisateur,
-      genre: film.genre,
-      dateDeSortie: film.dateDeSortie,
-      saison: film.saison,
-      numeroEpisode: film.numeroEpisode,
-      urlFilm: film.urlFilm,
-    );
-    _filmList.add(newFilm);
-    notifyListeners();
-  }
-}
+// class Movies with ChangeNotifier {
+//   List<Films> _filmList = [];
+//   void addFilm(Films film) {
+//     final newFilm = Films(
+//       id: DateTime.now().toString(),
+//       titre: film.titre,
+//       descrip: film.descrip,
+//       image: film.image,
+//       realisateur: film.realisateur,
+//       genre: film.genre,
+//       dateDeSortie: film.dateDeSortie,
+//       saison: film.saison,
+//       numeroEpisode: film.numeroEpisode,
+//       urlFilm: film.urlFilm,
+//     );
+//     _filmList.add(newFilm);
+//     notifyListeners();
+//   }
+// }
