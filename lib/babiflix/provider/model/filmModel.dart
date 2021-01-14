cff713f27fefbf0c17273e7dfa03d760414c30aa
pokou2020 <61155644+pokou2020@users.noreply.github.com>
2021-01-14 16:08:55 +0100

    commit

--- lib/babiflix/provider/model/filmModel.dart
+++ lib/babiflix/provider/model/filmModel.dart
@@ -9,57 +9,59 @@ Film filmFromMap(String str) => Film.fromMap(json.decode(str));
 String filmToMap(Film data) => json.encode(data.toMap());
 
 class Film {
-  Film({
-    this.id,
-    this.titre,
-    this.descrition,
-    this.image,
-    this.realisateur,
-    this.genreMovie,
-    this.isFilm,
-    this.dateSortie,
-    this.urlMovie,
-    this.numeroEpisode,
-    this.nomSerie,
-    this.numeroSaison,
-    this.casting,
-  });
+    Film({
+        this.id,
+        this.titre,
+        this.descrition,
+        this.image,
+        this.realisateur,
+        this.genreMovie,
+        this.isFilm,
+        this.dateSortie,
+        this.urlMovie,
+        this.numeroEpisode,
+        this.nomSerie,
+        this.numeroSaison,
+        this.nomPersonnage,
+        this.status,
+        this.saison,
+    });
 
-  String id;
-  String titre;
-  String descrition;
-  String image;
-  String realisateur;
-  GenreMovie genreMovie;
-  bool isFilm;
-  String dateSortie;
-  String urlMovie;
-  int numeroEpisode;
-  String nomSerie;
-  int numeroSaison;
-  String casting;
+    String id;
+    String titre;
+    String descrition;
+    String image;
+    String realisateur;
+    GenreMovie genreMovie;
+    bool isFilm;
+    String dateSortie;
+    String urlMovie;
+    int numeroEpisode;
+    String nomSerie;
+    int numeroSaison;
+    String nomPersonnage;
+    bool status;
+    Saison saison;
 
-  factory Film.fromMap(Map<String, dynamic> json) => Film(
+    factory Film.fromMap(Map<String, dynamic> json) => Film(
         id: json["id"] == null ? null : json["id"],
         titre: json["titre"] == null ? null : json["titre"],
         descrition: json["descrition"] == null ? null : json["descrition"],
         image: json["image"] == null ? null : json["image"],
         realisateur: json["realisateur"] == null ? null : json["realisateur"],
-        genreMovie: json["genreMovie"] == null
-            ? null
-            : GenreMovie.fromMap(json["genreMovie"]),
+        genreMovie: json["genreMovie"] == null ? null : GenreMovie.fromMap(json["genreMovie"]),
         isFilm: json["isFilm"] == null ? null : json["isFilm"],
         dateSortie: json["dateSortie"] == null ? null : json["dateSortie"],
         urlMovie: json["urlMovie"] == null ? null : json["urlMovie"],
-        numeroEpisode:
-            json["numeroEpisode"] == null ? null : json["numeroEpisode"],
+        numeroEpisode: json["numeroEpisode"] == null ? null : json["numeroEpisode"],
         nomSerie: json["nomSerie"] == null ? null : json["nomSerie"],
-        numeroSaison:
-            json["numeroSaison"] == null ? null : json["numeroSaison"],
-        casting: json["casting"] == null ? null : json["casting"],
-      );
+        numeroSaison: json["numeroSaison"] == null ? null : json["numeroSaison"],
+        nomPersonnage: json["nomPersonnage"] == null ? null : json["nomPersonnage"],
+        status: json["status"] == null ? null : json["status"],
+        saison: json["saison"] == null ? null : Saison.fromMap(json["saison"]),
+    );
 
-  Map<String, dynamic> toMap() => {
+    Map<String, dynamic> toMap() => {
         "id": id == null ? null : id,
         "titre": titre == null ? null : titre,
         "descrition": descrition == null ? null : descrition,
@@ -72,26 +74,64 @@ class Film {
         "numeroEpisode": numeroEpisode == null ? null : numeroEpisode,
         "nomSerie": nomSerie == null ? null : nomSerie,
         "numeroSaison": numeroSaison == null ? null : numeroSaison,
-        "casting": casting == null ? null : casting,
-      };
+        "nomPersonnage": nomPersonnage == null ? null : nomPersonnage,
+        "status": status == null ? null : status,
+        "saison": saison == null ? null : saison.toMap(),
+    };
 }
 
 class GenreMovie {
-  GenreMovie({
-    this.id,
-    this.titre,
-  });
+    GenreMovie({
+        this.id,
+        this.titre,
+    });
 
-  String id;
-  String titre;
+    String id;
+    String titre;
 
-  factory GenreMovie.fromMap(Map<String, dynamic> json) => GenreMovie(
+    factory GenreMovie.fromMap(Map<String, dynamic> json) => GenreMovie(
         id: json["id"] == null ? null : json["id"],
         titre: json["titre"] == null ? null : json["titre"],
-      );
+    );
 
-  Map<String, dynamic> toMap() => {
+    Map<String, dynamic> toMap() => {
         "id": id == null ? null : id,
         "titre": titre == null ? null : titre,
-      };
+    };
+}
+
+class Saison {
+    Saison({
+        this.id,
+        this.titre,
+        this.nombreEpisode,
+        this.numeroSaison,
+        this.image,
+        this.serieSaison,
+    });
+
+    String id;
+    String titre;
+    int nombreEpisode;
+    int numeroSaison;
+    String image;
+    String serieSaison;
+
+    factory Saison.fromMap(Map<String, dynamic> json) => Saison(
+        id: json["id"] == null ? null : json["id"],
+        titre: json["titre"] == null ? null : json["titre"],
+        nombreEpisode: json["nombreEpisode"] == null ? null : json["nombreEpisode"],
+        numeroSaison: json["numeroSaison"] == null ? null : json["numeroSaison"],
+        image: json["image"] == null ? null : json["image"],
+        serieSaison: json["serieSaison"] == null ? null : json["serieSaison"],
+    );
+
+    Map<String, dynamic> toMap() => {
+        "id": id == null ? null : id,
+        "titre": titre == null ? null : titre,
+        "nombreEpisode": nombreEpisode == null ? null : nombreEpisode,
+        "numeroSaison": numeroSaison == null ? null : numeroSaison,
+        "image": image == null ? null : image,
+        "serieSaison": serieSaison == null ? null : serieSaison,
+    };
 }
---
+++ lib/babiflix/screen/date.dart
@@ -0,0 +1,26 @@
+// import 'dart:convert';
+// import 'dart:ffi';
+
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart';
+// import 'package:intl/intl.dart';
+
+// class Time{
+// String time;
+
+// Time({this.time});
+
+// Future<Void>getTime() async{
+//   Response response= await get("https://babiflixdinamisation-default-rtdb.firebaseio.com/film.json");
+//   Map data=jsonDecode(response.body);
+//   print(data);
+
+
+//   DateTime ladate=DateTime.parse("12/12:2012");
+//   time=DateFormat.jm().format(ladate);
+// print(ladate);
+// }
+
+// }
+
+
