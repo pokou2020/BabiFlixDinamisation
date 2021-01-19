9fb1fec25e87de8cb1c6e675c01ff92e31baa423
syyao <31316365+syyao@users.noreply.github.com>
2021-01-19 16:11:41 +0000

    autre

--- lib/babiflix/screen/Accueil1.dart
+++ lib/babiflix/screen/Accueil1.dart
@@ -557,7 +557,7 @@ class _MainBodyState extends State<MainBody> {
                     padding: const EdgeInsets.all(10),
                     child: Container(
                       child: Text(
-                        "Comique",
+                        "Comiques",
                         style: TextStyle(color: Colors.white, fontSize: 18),
                       ),
                     ),
